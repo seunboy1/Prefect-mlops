93b49a8b7becff55c35c6bded792555cd6b9e166
Oluwaseun Adeyo <johnadeyo@hotmail.com>
2024-03-18 10:46:53 +0100

    restructuring

--- 3.4/cat_dog_facts.py
+++ 3.4/cat_dog_facts.py
@@ -25,7 +25,7 @@ def animal_facts():
 
     cat_fact = fetch_cat_fact()
     dog_fact = fetch_dog_fact()
-    print(f"🐱: {cat_fact} \n🐶: {dog_fact}")
+    print(f"🐱: {cat_fact} \n🐶  : {dog_fact}")
 
 if __name__ == "__main__":
     animal_facts()
